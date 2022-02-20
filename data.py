3dcf97fe819480adb7fa69f913b6c59b6ad018e8
idoyudha <idowidya.yudhatama@gmail.com>
2022-02-20 22:48:53 +0700

    refactor

---
+++ data.py
@@ -0,0 +1,38 @@
+database = [
+    {'ID': '0901', 'nama': 'Yoga Permana', 'lantai': 9, 'jabatan': 'Staf pemadam kebakaran', 'telepon': '081287659807'},
+    {'ID': '1201', 'nama': 'Iip Dharmawan', 'lantai': 12, 'jabatan': 'kepala staf first AID', 'telepon': '085476895430'}
+]
+
+list_main_menu = {
+    "0": "***** DATA KONTAK DARURAT GEDUNG DSP *****",
+    "1": "Tampilkan Kontak Petugas Emergency",
+    "2": "Tambahkan Data Petugas Emergency",
+    "3": "Rubah Data Petugas Emergency",
+    "4": "Hapus Data Petugas Emergency",
+    "5": "Exit",
+}
+
+list_contact_menu = {
+    "0": "'===== DATA KONTAK PETUGAS EMERGENCY =====",
+    "1": "Tampilkan Kontak Seluruh Petugas",
+    "2": "Tampilkan Kontak Petugas di lantai tertentu",
+    "3": "Kembali ke Menu Utama",
+}
+
+list_add_menu = {
+    "0": "+++++ MENAMBAH DATA PETUGAS EMERGENCY +++++",
+    "1": "Tambah Data Petugas",
+    "2": "Kembali ke Menu Utama",
+}
+
+list_replace_menu = {
+    "0": ">>>>> MENGUBAH DATA PETUGAS EMERGENCY <<<<<",
+    "1": "Ubah Data Petugas",
+    "2": "Kembali ke Menu Utama",
+}
+
+list_delete_menu = {
+    "0": "----- MENGHAPUS DATA PETUGAS EMERGENCY -----",
+    "1": "Hapus Data Petugas",
+    "2": "Kembali ke Menu Utama",
+}
